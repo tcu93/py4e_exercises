b136788f85f5e5f7b4c21e0b69d5a903f3f0c87c
tcu93 <tcu93@live.com>
2019-10-10 14:30:45 -0500

    initial commit

---
+++ 2vars2.py
@@ -0,0 +1,8 @@
+'''Exercise 2   Write a program that uses raw_input to prompt 
+a user for their name and then welcomes them.
+'''
+
+name = input('What is your name?\n')
+print(f'welcome {name}')
+
+
